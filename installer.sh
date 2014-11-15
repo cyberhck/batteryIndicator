1ffcb10080cd3bc9376862cbc79d5e91e06ec8b7
Nishchal Gautam <gautam.nishchal@gmail.com>
2014-11-15 08:22:17 +0530

    installer thing

--- installer.sh
+++ installer.sh
@@ -1,1 +1,3 @@
+echo "Make sure you have changed your shell to zsh and you're executing this file as root user (use sudo), if not this query will fail, please quit during this 3 second pause (Ctrl+C)"
+sleep 3
 apt-get install acpi
